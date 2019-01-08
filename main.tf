69a708acfc91c4b13ec73015827f66ed98f6662a
Slav Donchev <35224472+slavrd@users.noreply.github.com>
2019-01-08 21:32:20 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,8 +4,8 @@ resource "null_resource" "pwsh" {
 wget -q https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb
 export DEBIAN_FRONTEND=noninteractive
 sudo dpkg -i packages-microsoft-prod.deb
-sudo apt-get update
-sudo apt-get install -y powershell
+sudo apt-get update >/dev/null
+sudo apt-get install -y powershell >/dev/null
 sudo pwsh -command "Install-Module -Name SqlServer -Force"
 EOH
   }
