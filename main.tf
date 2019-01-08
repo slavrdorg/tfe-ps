9f5169d24d363d53ae13b281afe011babee54997
Slav Donchev <35224472+slavrd@users.noreply.github.com>
2019-01-08 21:37:14 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,8 +2,7 @@ resource "null_resource" "pwsh" {
   provisioner "local-exec" {
     command = <<EOH
 wget -q https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb
-export DEBIAN_FRONTEND=noninteractive
-sudo dpkg -i packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb >/dev/null
 sudo apt-get update >/dev/null
 sudo apt-get install -y powershell >/dev/null
 sudo pwsh -command "Install-Module -Name SqlServer -Force"
